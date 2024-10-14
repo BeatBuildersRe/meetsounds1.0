d2524da9c6ac946dbee658947800661dc457594c
Hoselot <joaquincangialosidp@gmail.com>
2024-10-13 21:11:24 -0300

    feat: Ya se registran usuarios con fecha de nacimiento

---
+++ backend/src/main/java/com/project/meetsounds/config/GraphQLConfig.java
@@ -0,0 +1,19 @@
+package com.project.meetsounds.config;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.GraphQLScalarType;
+import graphql.scalars.ExtendedScalars;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+@Configuration
+public class GraphQLConfig {
+
+    @Bean
+    public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+        return wiringBuilder -> wiringBuilder
+                .scalar(ExtendedScalars.Date)
+                .scalar(ExtendedScalars.DateTime);  // Si también usas DateTime
+    }
+}
+
