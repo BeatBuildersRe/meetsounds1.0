4ddc91d13b97cac99d6a1a887266df45593f3071
Mauro Berni <mauroberni002@gmail.com>
2024-10-15 15:53:49 -0300

    [refactor] Cambios en publicaciones

--- frontend/src/components/Publicaciones/Galeria.jsx
+++ frontend/src/components/Publicaciones/Galeria.jsx
@@ -6,22 +6,25 @@ import img1 from '@public/imgtest.png'
 import img2 from '@public/imgtest2.png'
 import img3 from '@public/imgtest3.png'
 import img4 from '@public/imgtest4.png'
-const ImageGallery = () => {
-    const maxImagesToShow = 4;
-    const images = [img1, img2,img3]
+const ImageGallery = ({ images = [] }) => {
+    const cantidadImagenes = images.length;
     return (
         <>
             <div className="container-w">
-                <div className={`galeria-imagenes-${images.length}`}>
-                    {images.slice(0, maxImagesToShow).map((image, index) => (
+                <div className={`galeria-imagenes-${cantidadImagenes}`}>
+                    {/* Asegúrate de que images es un array y contiene al menos un elemento */}
+                    {Array.isArray(images) && cantidadImagenes > 0 && images.slice(0, 4).map((image, index) => (
                         <div key={index} className='imagen-contenedor' id={`img-${index}`}>
-                            <img src={image} alt="" id='img' className={`img-${index}`} />
-                            {images.length > 4 && (
-                                <h4 id='h4-mas'> No me toquen el CSS de este componente o los <GiDeathSkull/>+ {images.length - 4}</h4>
-                            )}
+                            <img src={image} alt={`Imagen ${index}`} id='img' className={`img-${index}`} />
                         </div>
-
                     ))}
+
+                    {/* Si hay más de 4 imágenes, muestra el mensaje */}
+                    {cantidadImagenes > 4 && (
+                        <h4 id='h4-mas'>
+                            No me toquen el CSS de este componente o los <GiDeathSkull /> +{cantidadImagenes - 4}
+                        </h4>
+                    )}
                 </div>
             </div>
         </>
