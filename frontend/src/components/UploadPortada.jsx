eaba754de01764c8cb14f30b9c53542fe23cecf6
EmiArias021 <emilianoariasjesus86@gmail.com>
2024-10-31 10:50:35 -0300

    feat: cambios en los upload y modal del perfil nuevo

--- frontend/src/components/UploadPortada.jsx
+++ frontend/src/components/UploadPortada.jsx
@@ -1,45 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Avatars from '@c/avatar/AvatarV2';
-import useUpdateImgPerfil from '@services/UpdatePerfil';
-
 import Avatar from 'react-avatar-edit';
-import foto from '@public/perfill.png';
-import '@css/UploadAvatar.css';
 import BotonPlus from '@c/botones/BotonPlus';
+
 const UploadPortada = ({ btn_cancelar, onImageSave, Imagen, Portada, alias }) => {
-    const [src, setSrc] = useState(null);
     const [preview, setPreview] = useState(Portada);
-    const [savedImage, setSavedImage] = useState(null); // Imagen guardada
 
     const onClose = () => {
-        setPreview(Portada); // Restaurar la imagen por defecto si se cierra el editor
+        setPreview(Portada);
     };
 
     const onCrop = (view) => {
-        setPreview(view); // Actualizar la vista previa con la imagen recortada
+        setPreview(view);
     };
-
-    useEffect(() => {
-    }, [preview]);
 
     const handleSave = () => {
-        setSavedImage(preview); // Guarda la imagen recortada localmente
-        onImageSave(preview); // Enviar la imagen recortada al componente padre
-        useUpdateImgPerfil(preview,alias,2) // envia a la bd
+        onImageSave(preview); // Only pass the preview to the parent component
+        btn_cancelar(); // Close the upload component
     };
-
 
     return (
         <div className='UploadAvatar'>
-            
             <div className='Up'>
                 <div className='media'>
                     <div className="Portada">
-                        {preview &&  <img id="Portada" src={preview} alt="" />}
+                        {preview && <img id="Portada" src={preview} alt="" />}
                         <Avatars class='Perfil' imagen={Imagen} width={100} height={100} />
                     </div>
                 </div>
-
             </div>
 
             <div className='avatar'>
@@ -49,12 +37,10 @@ const UploadPortada = ({ btn_cancelar, onImageSave, Imagen, Portada, alias }) =>
                     onCrop={onCrop}
                     onClose={onClose}
                     labelStyle={{ color: 'red' }}
-                    src={src}
                     closeIconColor='red'
                     mimeTypes='.jpeg,.jpg,.png'
                     label='Toca para subir una imagen'
                     exportAsSquare={true}
-
                 />
             </div>
 
@@ -65,13 +51,8 @@ const UploadPortada = ({ btn_cancelar, onImageSave, Imagen, Portada, alias }) =>
                 <div id='Cancelar' onClick={btn_cancelar}>
                     <BotonPlus text='Salir' color='red' />
                 </div>
-
-
-                {/* <button id="btn-guardar" onClick={() => { handleSave(); btn_cancelar(); }}>Guardar Imagen</button> */}
-                {/* <button id="btn-cerrar" onClick={btn_cancelar}>cerrar</button> */}
             </div>
         </div>
-
     );
 };
 
