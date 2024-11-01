1290ef89fba43d8256319cc06228207f2177d838
Germán Alcaino <germanalcaino789@gmail.com>
2024-11-01 18:03:33 -0300

    Resuelvo error

--- frontend/src/Pages/Mensajes/Mensajes.jsx
+++ frontend/src/Pages/Mensajes/Mensajes.jsx
@@ -1,17 +1,21 @@
-import * as React from 'react';
+import MenuDerechoDiv from "@c/Menu/Derecha";
 import '@css/Mensajes.css'
-import MenuDerechoDiv from '../Home/Derecha';
-const Mensajes = () => {
+const Bandas = () => {
     return (
         <>
             <div className="Contenedor">
                 <div className="contenedor2">
+                    <div className="izquierda-mensajes">
 
+                    {/* AREA DE TRABAJO */}
+
+                    </div>
                     <MenuDerechoDiv></MenuDerechoDiv>
                 </div>
             </div>
             <div />
         </>
+
     )
 }
-export default Mensajes;
+export default Bandas;
