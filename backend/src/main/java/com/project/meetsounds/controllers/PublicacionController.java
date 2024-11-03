c0616e454514716837c979774f0130a305f4c800
Leonardo <leonardoberni0@gmail.com>
2024-11-03 17:14:54 -0300

    Arreglos en el codigo

--- backend/src/main/java/com/project/meetsounds/controllers/PublicacionController.java
+++ backend/src/main/java/com/project/meetsounds/controllers/PublicacionController.java
@@ -1,6 +1,6 @@
 package com.project.meetsounds.controllers;
 
-import com.project.meetsounds.domain.models.ComentarioOut;
+
 import com.project.meetsounds.domain.models.Publicacion;
 
 import com.project.meetsounds.domain.models.PublicacionOut;
@@ -10,14 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class PublicacionController {
 
     @Autowired
