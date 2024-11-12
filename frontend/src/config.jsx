c9ee9f3bbfbe01416243c0aa2716376b7a04ca90
Hoselot <joaquincangialosidp@gmail.com>
2024-11-12 13:12:53 -0300

    fix: prueba funcionamiento back y front deploy

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:8080';
-export const BASE_URL_SOCKET = 'ws://localhost:8080';
+export const BASE_URL = 'https://meetsoundsbackapp.fly.dev/';
+export const BASE_URL_SOCKET = 'ws://meetsoundsbackapp.fly.dev/';
