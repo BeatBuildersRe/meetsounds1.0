32edf683d68bb4bf076b0745edc473d6064f5d10
Hoselot <joaquincangialosidp@gmail.com>
2024-11-12 13:36:56 -0300

    prueba conexion

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://meetsoundsbackapp.fly.dev/';
-export const BASE_URL_SOCKET = 'wss://meetsoundsbackapp.fly.dev/';
+export const BASE_URL = 'https://meetsoundsbackapp.fly.dev';
+export const BASE_URL_SOCKET = 'wss://meetsoundsbackapp.fly.dev';
