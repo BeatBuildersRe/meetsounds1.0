370690a8c6afcc33018410122ecb3e062b478600
EmiArias021 <emilianoariasjesus86@gmail.com>
2024-11-13 15:54:41 -0300

    feat: arreglo

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'https://meetsoundsbackapp.fly.dev';
-// export const BASE_URL_SOCKET = 'wss://meetsoundsbackapp.fly.dev';
+export const BASE_URL = 'https://meetsoundsbackapp.fly.dev';
+export const BASE_URL_SOCKET = 'wss://meetsoundsbackapp.fly.dev';
 
-export const BASE_URL = 'http://localhost:8080';
-export const BASE_URL_SOCKET = 'ws://localhost:8080';
+// export const BASE_URL = 'http://localhost:8080';
+// export const BASE_URL_SOCKET = 'ws://localhost:8080';
